ef269e832b85267e0d2327e6c25b2e128c8c3d8d
valkyrie22fi <admin@afigol.ru>
2024-08-13 19:00:34 +0300

    Внесение правок от ревьюера

--- scripts/index.js
+++ scripts/index.js
@@ -16,7 +16,7 @@ function createCard(name, link, deleteCallback) {
   cardElement.querySelector('.card__image').alt = name;
 
   deleteButton.addEventListener('click', () => {
-    deleteCard(cardElement)
+    deleteCallback(cardElement)
   });
   
   return cardElement;
@@ -25,7 +25,7 @@ function createCard(name, link, deleteCallback) {
 // @todo: Вывести карточки на страницу
 function addCardList() {
   initialCards.forEach((el) => {
-    cardsList.append(addCards(el.name, el.link)); 
+    cardsList.append(createCard(el.name, el.link, deleteCard))
   })
 };
 
