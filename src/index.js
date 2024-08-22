b3a2818a1eebe5910971e1cfb816fbaf6fc43529
valkyrie22fi <admin@afigol.ru>
2024-08-22 21:41:39 +0300

    Настройка сборки Webpack и cоздание файла .gitignore

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import './pages/index.css';
+
+// import jordanImage from './images/jordan.jpg';
+// import jamesImage from './images/james.jpg';
+// import bryantImage from './images/bryant.jpg';
+
+// const whoIsTheGoat = [
+//   // меняем исходные пути на переменные
+//   { name: 'Michael Jordan', link: jordanImage },
+//   { name: 'Lebron James', link: jamesImage },
+//   { name: 'Kobe Bryant', link: bryantImage },
+// ];
