dc4dc9acd6eee7bd9787cfdbf63afc8267eefdc0
valkyrie22fi <admin@afigol.ru>
2024-11-05 12:28:09 +0300

    Рефакторинг кода

--- src/components/popupTypeImage.js
+++ src/components/popupTypeImage.js
@@ -1,4 +1,4 @@
-import { openModal, closeModal, closePopupOverlay} from '../index.js'
+import { openModal, closeModal } from '../index.js'
 
 // Модальное окно картинки
 const popupTypeImage = document.querySelector('.popup_type_image');
@@ -11,12 +11,12 @@ function closePopupTypeImage() {
   });
 }
 
-function openImage(item, link, name) {
+function openImage(link, name) {
   "открыть попап картинки"
-  openModal(item);
-  item.querySelector('.popup__image').src = link;
-  item.querySelector('.popup__image').alt = name; 
-  item.querySelector('.popup__caption').textContent = name;
+  openModal(popupTypeImage);
+  popupTypeImage.querySelector('.popup__image').src = link;
+  popupTypeImage.querySelector('.popup__image').alt = name; 
+  popupTypeImage.querySelector('.popup__caption').textContent = name;
 }
 
-export { openImage, closePopupTypeImage, popupTypeImage }
+export { openImage, closePopupTypeImage }
