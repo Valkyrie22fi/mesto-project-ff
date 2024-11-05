a14415b09009bd6fea604ba87fcbbff4358cb06a
valkyrie22fi <admin@afigol.ru>
2024-11-05 13:55:22 +0300

    Создание переменных cardTitle и cardImage, переименование let

--- src/components/card.js
+++ src/components/card.js
@@ -3,11 +3,13 @@ const cardTemplate = document.querySelector('#card-template').content;
 
 // создать и вернуть элемент карточки
 function createCard(name, link, deleteCallback) {
-  const cardElement = cardTemplate.querySelector('.card').cloneNode(true); 
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  const cardTitle = cardElement.querySelector('.card__title')
+  const cardImage = cardElement.querySelector('.card__image')
   const deleteButton = cardElement.querySelector('.card__delete-button'); 
-  cardElement.querySelector('.card__title').textContent = name; 
-  cardElement.querySelector('.card__image').src = link; 
-  cardElement.querySelector('.card__image').alt = name;
+  cardTitle.textContent = name; 
+  cardImage.src = link; 
+  cardImage.alt = name;
  
   deleteButton.addEventListener('click', () => { 
     deleteCallback(cardElement) 
