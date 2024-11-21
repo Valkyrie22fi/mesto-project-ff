c38edc1c8cdf91fe97114c1d52c418be6f4a0449
valkyrie22fi <admin@afigol.ru>
2024-11-21 19:06:42 +0300

    Внесение правок в файл modal.js

--- src/components/modal.js
+++ src/components/modal.js
@@ -2,25 +2,26 @@
 export function openModal(item) {
   item.classList.add('popup_is-opened', 'popup_is-animated');
   item.addEventListener('click', closePopupOverlay); 
-  
   document.addEventListener('keydown', closePopupEsc);
 }
 
 // закрыть попап
 export function closeModal(item) {
   item.classList.remove('popup_is-opened');
-  item.removeEventListener('keydown', closePopupEsc);
   item.removeEventListener('click', closePopupOverlay);
+  document.removeEventListener('keydown', closePopupEsc);
 }
 
 // закрыть попап кликом по оверлей
 function closePopupOverlay(evt) {
-  evt.target.classList.remove('popup_is-opened');
+  if(evt.target.classList.contains('popup_is-opened')) {
+    closeModal(evt.target)
+  }
 }
 
 // закрыть попап по нажатию на Esc
 function closePopupEsc(evt) {
   if(evt.key === 'Escape') {
-    document.querySelector('.popup_is-opened').classList.remove('popup_is-opened');
+    closeModal(document.querySelector('.popup_is-opened'))
   }
 }
