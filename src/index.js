e4e2be6d67353f6e3d416f48e45d7ad8c123f04a
valkyrie22fi <admin@afigol.ru>
2024-12-03 17:35:06 +0300

    Валидация формы Редактировать профиль

--- src/index.js
+++ src/index.js
@@ -106,3 +106,80 @@ closeButtonPopupTypeImage.addEventListener('click', function () {
   closeModal(popupTypeImage);
 });
 
+// Валидация
+const formElement = document.querySelector('.popup__form');
+const formInput = formElement.querySelector('.popup__input');
+const formError = formElement.querySelector(`.${formInput.id}-error`);
+
+const showError = (formElement, inputElement, errorMessage) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.add('popup__input_type_error');
+  if (inputElement.validity.patternMismatch) {
+    errorElement.textContent = "Может содержать только латинские и кириллические буквы, знаки дефиса и пробелы"
+  } else {
+    errorElement.textContent = errorMessage;
+  }
+  errorElement.classList.add('popup__input-error_active');
+};
+
+const hideError = (formElement, inputElement) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.remove('popup__input_type_error');
+  errorElement.classList.remove('popup__input-error_active');
+  errorElement.textContent = '';
+};
+
+const checkInputValidity = (formElement, inputElement) => {
+  console.log(inputElement.validity.valid)
+  if (!inputElement.validity.valid) {
+    showError(formElement, inputElement, inputElement.validationMessage);
+  } else {
+    hideError(formElement, inputElement);
+  }
+};
+
+const hasInvalidInput = (inputList) => {
+  return inputList.some((inputElement) => {
+    return !inputElement.validity.valid;
+  })
+}; 
+
+const toggleButtonState = (inputList, buttonElement) => {
+  if (hasInvalidInput(inputList)) {
+    
+    buttonElement.disabled = true;
+    buttonElement.classList.add('button_inactive');
+  } else {
+    buttonElement.disabled = false;
+    buttonElement.classList.remove('button_inactive');
+  }
+}; 
+
+const setEventListeners = (formElement) => {
+  const inputList = Array.from(formElement.querySelectorAll('.popup__input'));
+  const buttonElement = formElement.querySelector('.popup__button');
+  console.log(inputList)
+  toggleButtonState(inputList, buttonElement);
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener('input', function () {
+      checkInputValidity(formElement, inputElement);
+      toggleButtonState(inputList, buttonElement);
+    });
+  });
+};
+
+const enableValidation = () => {
+  const formList = Array.from(document.querySelectorAll('.popup__form'));
+  formList.forEach((formElement) => {
+    formElement.addEventListener('submit', function (evt) {
+      evt.preventDefault();
+    });
+    const fieldsetList = Array.from(formElement.querySelectorAll('.popup__set'));
+
+    fieldsetList.forEach((fieldSet) => {
+      setEventListeners(fieldSet);
+    });
+  });
+};
+
+enableValidation();
